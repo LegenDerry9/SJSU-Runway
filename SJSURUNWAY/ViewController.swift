401f8ae5fe0f697931461ff34672333358e8d028
MichaelOng <ongjmichael@gmail.com>
2018-04-25 18:44:31 -0700

    Initial Commit

---
+++ SJSURUNWAY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SJSURUNWAY
+//
+//  Created by Michael Ong on 4/25/18.
+//  Copyright © 2018 Michael Ong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
